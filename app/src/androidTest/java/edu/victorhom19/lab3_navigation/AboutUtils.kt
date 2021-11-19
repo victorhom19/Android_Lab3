ba45ed6f57e1fb6db8d494487f135ac0ae02f758
victorhom19 <homyakov.vs@edu.spbstu.ru>
2021-11-20 00:07:12 +0300

    Added UI testing (missed some files)

---
+++ app/src/androidTest/java/edu/victorhom19/lab3_navigation/AboutUtils.kt
@@ -0,0 +1,20 @@
+package edu.victorhom19.lab3_navigation
+
+import android.view.Gravity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openContextualActionModeOverflowMenu
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+
+private fun openAboutViaOptions() {
+    openContextualActionModeOverflowMenu()
+    onView(withText("Activity About"))
+        .perform(click())
+}
+
+fun openAbout() = openAboutViaOptions()
